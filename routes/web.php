ea0e2b07f0b9454f7b910169edc35404304a5053
gitName <bartk@list.ru>
2025-04-02 20:10:20 +0300

    chore: add /rickmortyapi route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\RickMortyController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get( '/rickmortyapi', [RickMortyController::class, 'index']);
