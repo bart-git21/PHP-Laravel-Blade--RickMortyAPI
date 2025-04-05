9fde94d2846a0f73cd400379020fbf80c2093737
gitName <bartk@list.ru>
2025-04-05 21:41:44 +0300

    refactor: use laravel queue with InsertEpisodeJob

--- app/Http/Controllers/RickMortyController.php
+++ app/Http/Controllers/RickMortyController.php
@@ -3,20 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Http;
+use App\Jobs\InsertEpisodesJob;
 
 class RickMortyController extends Controller
 {
-    public function index($id)
+    public function index()
     {
-        $response = Http::withOptions([
-            'verify' => false,
-        ])->get("https://rickandmortyapi.com/api/character/?page=$id");
-        $characters = $response->json();
-
-        if ($characters) {
-            return view('home')->with(['buttonHidden' => true, 'characters' => $characters['results']]);
-        } else {
-            return view('home')->with(['buttonHidden' => false, 'error' => $response->json()]);
-        }
+        // 
+    }
+    public function store()
+    {
+        InsertEpisodesJob::dispatch();
+        return redirect()->route('root');
     }
 }
