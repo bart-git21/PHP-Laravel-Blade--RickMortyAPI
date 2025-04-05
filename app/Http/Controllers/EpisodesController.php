1701eb0f8b75ba4ac12692cbc4bd9769a47ce92a
gitName <bartk@list.ru>
2025-04-05 18:10:25 +0300

    refactor: episodes controller store() function insert all episodes from external API into the local episodes table

--- app/Http/Controllers/EpisodesController.php
+++ app/Http/Controllers/EpisodesController.php
@@ -14,20 +14,22 @@ class EpisodesController extends Controller
     }
     public function store(Request $request)
     {
-        $id = $request['getEpisode'];
-        $response = Http::withOptions(['verify' => false])->get("https://rickandmortyapi.com/api/episode/$id");
+        $response = Http::withOptions(['verify' => false])->get("https://rickandmortyapi.com/api/episode/");
         $json = $response->json();
-        $json['characters'] = str_replace('https://rickandmortyapi.com/api/character/', '', $json['characters']);
-        foreach ($json['characters'] as $key => $character) {
-            $json['characters'][$key] = intval($character);
+        $results = $json['results'];
+        foreach ($results as $result) {
+            $result['characters'] = str_replace('https://rickandmortyapi.com/api/character/', '', $result['characters']);
+            foreach ($result['characters'] as $key => $character) {
+                $result['characters'][$key] = intval($character);
+            }
+            $episodes = new Episodes();
+            $episodes->episode_id = $result['id'];
+            $episodes->name = $result['name'];
+            $episodes->url = $result['url'];
+            $episodes->air_date = $result['air_date'];
+            $episodes->characters = $result['characters'];
+            $episodes->save();
         }
-        $episodes = new Episodes();
-        $episodes->episode_id = $json['id'];
-        $episodes->name = $json['name'];
-        $episodes->url = $json['url'];
-        $episodes->air_date = $json['air_date'];
-        $episodes->characters = $json['characters'];
-        $episodes->save();
         return redirect()->route('root');
     }
 }
