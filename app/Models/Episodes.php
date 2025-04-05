5bf91b05d3c2dbe41196bd80b8b15b02c07f0783
gitName <bartk@list.ru>
2025-04-05 17:39:46 +0300

    refactor: define an array type for characters column of the episodes table

--- app/Models/Episodes.php
+++ app/Models/Episodes.php
@@ -9,4 +9,8 @@ class episodes extends Model
     protected $table = 'episode_test';
 
     protected $fillable = ['eposide_id', 'name', 'url', 'air_date', 'characters'];
+
+    protected $casts = [
+        'characters' => 'array',
+    ];
 }
