a248f9c7af0552305e343ee0a83a22761997be25
gitName <bartk@list.ru>
2025-04-05 21:39:25 +0300

    feat: define handle function inside the InsertEpisodesJob; get data from external API and insert it to the episodes table

--- app/Jobs/insertEpisodesJob.php
+++ app/Jobs/insertEpisodesJob.php
@@ -4,6 +4,8 @@ namespace App\Jobs;
 
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\Http;
+use App\Models\Episodes;
 
 class insertEpisodesJob implements ShouldQueue
 {
@@ -22,6 +24,21 @@ class insertEpisodesJob implements ShouldQueue
      */
     public function handle(): void
     {
-        //
+        $response = Http::withOptions(['verify' => false])->get("https://rickandmortyapi.com/api/episode/");
+        $json = $response->json();
+        $results = $json['results'];
+        foreach ($results as $result) {
+            $result['characters'] = str_replace('https://rickandmortyapi.com/api/character/', '', $result['characters']);
+            foreach ($result['characters'] as $key => $character) {
+                $result['characters'][$key] = intval($character);
+            }
+            $episodes = new Episodes();
+            $episodes->episode_id = $result['id'];
+            $episodes->name = $result['name'];
+            $episodes->url = $result['url'];
+            $episodes->air_date = $result['air_date'];
+            $episodes->characters = $result['characters'];
+            $episodes->save();
+        }
     }
 }
