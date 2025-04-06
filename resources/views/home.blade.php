e7a57f60765d0124a0d7ac4e865b18d909f83004
gitName <bartk@list.ru>
2025-04-06 18:27:16 +0300

    feat: get filtered characters

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -31,11 +31,14 @@
 
 <body>
     @php
-        $characters = DB::table('character_test')->get();
+        $characters = DB::table('character_test')
+            ->select('character_test.*', 'location_test.location_name')
+            ->leftJoin('location_test', 'character_test.location_id', '=', 'location_test.location_id')
+            ->whereBetween('character_test.character_id', [2,3])
+            ->get();
     @endphp
 
     <div class="container">
-
         @if (count($characters))
             <h1>Characters</h1>
             <table class="table table-dark table-striped table-hover table-bordered border-primary">
@@ -47,25 +50,23 @@
                     <th>Episodes</th>
                 </thead>
                 @foreach($characters as $key => $single)
-                    @if ($key < 10 && $key >= 0)
-                        <tr>
-                            <td>{{ $single->character_id }}</td>
-                            <td class="p-0 w-90">
-                                <a class="text-decoration-none"
-                                    href="https://rickandmortyapi.com/api/episode/{{ $single->character_id }}">
-                                    <div class="card text-center" style="width: 10rem;">
-                                        <img class="card-img-top" src="{{ $single->img_href }}" alt="">
-                                        <div class="card-body">
-                                            <h5 class="card-title">{{ $single->name }}</h5>
-                                        </div>
+                    <tr>
+                        <td>{{ $single->character_id }}</td>
+                        <td class="p-0 w-90">
+                            <a class="text-decoration-none"
+                                href="https://rickandmortyapi.com/api/episode/{{ $single->character_id }}">
+                                <div class="card text-center" style="width: 10rem;">
+                                    <img class="card-img-top" src="{{ $single->img_href }}" alt="">
+                                    <div class="card-body">
+                                        <h5 class="card-title">{{ $single->name }}</h5>
                                     </div>
-                                </a>
-                            </td>
-                            <td>{{ $single->status }}</td>
-                            <td>{{ $single->location_id }}</td>
-                            <td>{{ $single->episodes_id }}</td>
-                        </tr>
-                    @endif
+                                </div>
+                            </a>
+                        </td>
+                        <td>{{ $single->status }}</td>
+                        <td>{{ $single->location_name }}</td>
+                        <td>{{ $single->episodes_id }}</td>
+                    </tr>
                 @endforeach
             </table>
         @else
