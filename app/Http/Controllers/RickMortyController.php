6502bc13eadb072b5510f738e7a4be7190e340b4
gitName <bartk@list.ru>
2025-04-06 10:25:31 +0300

    refactor: dispatch the location insert job from rickmorty controller store()

--- app/Http/Controllers/RickMortyController.php
+++ app/Http/Controllers/RickMortyController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Http;
+use App\Jobs\InsertLocationsJob;
 use App\Jobs\InsertEpisodesJob;
 
 class RickMortyController extends Controller
@@ -13,6 +14,7 @@ class RickMortyController extends Controller
     }
     public function store()
     {
+        InsertLocationsJob::dispatch();
         InsertEpisodesJob::dispatch();
         return redirect()->route('root');
     }
