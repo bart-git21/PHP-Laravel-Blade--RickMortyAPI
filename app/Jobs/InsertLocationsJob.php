60a589c71f97ad73dbdf8488a19e646f51c34a7a
gitName <bartk@list.ru>
2025-04-06 14:32:23 +0300

    refactor: modify handle() with getLocation($url) which recursice get external data

--- app/Jobs/InsertLocationsJob.php
+++ app/Jobs/InsertLocationsJob.php
@@ -13,13 +13,14 @@ class InsertLocationsJob implements ShouldQueue
 
     public $tries = 3;
     public $timeout = 120;
+    protected $url;
 
     /**
      * Create a new job instance.
      */
-    public function __construct()
+    public function __construct($url)
     {
-        //
+        $this->url = $url;
     }
 
     /**
@@ -27,21 +28,30 @@ class InsertLocationsJob implements ShouldQueue
      */
     public function handle(): void
     {
-        $response = Http::withOptions(['verify' => false])->get("https://rickandmortyapi.com/api/location/");
-        $json = $response->json();
-        $results = $json['results'];
+        function getLocation($url)
+        {
+            $response = Http::withOptions(['verify' => false])->get($url);
+            $json = $response->json();
+            $results = $json['results'];
 
-        foreach ($results as $result) {
-            $result['residents'] = str_replace('https://rickandmortyapi.com/api/character/', '', $result['residents']);
-            foreach ($result['residents'] as $key => $resident) {
-                $result['residents'][$key] = intval($resident);
+            foreach ($results as $result) {
+                $result['residents'] = str_replace('https://rickandmortyapi.com/api/character/', '', $result['residents']);
+                foreach ($result['residents'] as $key => $resident) {
+                    $result['residents'][$key] = intval($resident);
+                }
+                $locations = new Locations();
+                $locations->location_id = $result['id'];
+                $locations->location_name = $result['name'];
+                $locations->residents = $result['residents'];
+                $locations->location_url = $result['url'];
+                $locations->save();
             }
-            $locations = new Locations();
-            $locations->location_id = $result['id'];
-            $locations->location_name = $result['name'];
-            $locations->residents = $result['residents'];
-            $locations->location_url = $result['url'];
-            $locations->save();
+
+            $nextPage = $json['info']['next'];
+            if ($nextPage) {
+                getLocation($nextPage);
+            }
         }
+        getLocation($this->url);
     }
 }
