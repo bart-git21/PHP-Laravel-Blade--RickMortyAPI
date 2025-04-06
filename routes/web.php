553405e85da55a57b24b85794615a97f546a2378
gitName <bartk@list.ru>
2025-04-07 00:48:59 +0300

    feat: handle POST '/export' endpoint

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RickMortyController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ExportController;
 
 Route::get('/', [HomeController::class, 'index'])->name('root');
 Route::post('/', [HomeController::class, 'index']);
 Route::get('/rickmortyapi', [RickMortyController::class, 'store'])->name('rickmortyapi.store');
+Route::post('/export', [ExportController::class, 'export']);
