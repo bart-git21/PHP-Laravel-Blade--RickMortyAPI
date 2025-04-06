bb11e6e5526464a6cddf4a93d7f189efba588038
gitName <bartk@list.ru>
2025-04-06 09:49:39 +0300

    refactor: define locations model

--- app/Models/Locations.php
+++ app/Models/Locations.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Locations extends Model
 {
-    //
+    protected $table = 'location_test';
+
+    protected $fillable = ['location_id', 'location_name', 'residents', 'location_url'];
+
+    protected $casts = [
+        'residents' => 'array',
+    ];
 }
