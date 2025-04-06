16fd47cd61824d6ced775abc799b86ed348707ae
gitName <bartk@list.ru>
2025-04-06 21:29:14 +0300

    feat: add http POST '/' endpoint

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,5 @@ use App\Http\Controllers\RickMortyController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('root');
+Route::post('/', [HomeController::class, 'index']);
 Route::get('/rickmortyapi', [RickMortyController::class, 'store'])->name('rickmortyapi.store');
