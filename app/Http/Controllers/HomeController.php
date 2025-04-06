4e2258ca6ad3842ada11510f039dbdf05fbae294
gitName <bartk@list.ru>
2025-04-06 21:27:34 +0300

    feat: HomeController handle request parameters

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,9 @@ class HomeController extends Controller
 {
     public function index(Request $request)
     {
-        return view('home');
+        if ($request->name || $request->options || $request->location) {
+            return view('home', ['name' => $request->name, 'options' => $request->options, 'location' => $request->location]);
+        }
+        return view('home', ['name' => "", 'options' => "", 'location' => ""]);
     }
 }
