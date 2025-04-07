617724b4596e5c4b8a6e9d32c677526b5344c31d
gitName <bartk@list.ru>
2025-04-07 20:40:13 +0300

    fix: array syntax is correct

--- database/migrations/2025_04_05_110055_add_index_to_episode_id_in_episode_test_table.php
+++ database/migrations/2025_04_05_110055_add_index_to_episode_id_in_episode_test_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('episode_test', function (Blueprint $table) {
-            $table->dropIndex('episode_id');
+            $table->dropIndex(['episode_id']);
         });
     }
 };
