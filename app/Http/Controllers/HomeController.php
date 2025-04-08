7c9cc50a56487ed4fe28d73af7cf16f0ee7a6101
gitName <bartk@list.ru>
2025-04-08 23:28:59 +0300

    docs: use line breaks for better readability

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,9 +8,28 @@ class HomeController extends Controller
 {
     public function index(Request $request)
     {
-        if ($request->name || $request->episode || $request->options || $request->location) {
-            return view('home', ['name' => $request->name, 'episode' => $request->episode, 'options' => $request->options, 'location' => $request->location]);
+        if (
+            $request->offset
+            || $request->name
+            || $request->episode
+            || $request->options
+            || $request->location
+        ) {
+            $offset = $request->offset ?: 0;
+            return view('home', [
+                'offset' => $offset,
+                'name' => $request->name,
+                'episode' => $request->episode,
+                'options' => $request->options,
+                'location' => $request->location
+            ]);
         }
-        return view('home', ['name' => "", 'episode' => "", 'options' => "", 'location' => ""]);
+        return view('home', [
+            'offset' => 0,
+            'name' => "",
+            'episode' => "",
+            'options' => "",
+            'location' => ""
+        ]);
     }
 }
