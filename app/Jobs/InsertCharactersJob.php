97a5c54dedc519034ececb5e4a8795438b1c764f
gitName <bartk@list.ru>
2025-04-08 16:04:01 +0300

    feat: update job_statuses table to access client side reload() function if job is completed

--- app/Jobs/InsertCharactersJob.php
+++ app/Jobs/InsertCharactersJob.php
@@ -7,19 +7,22 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
 use Illuminate\Support\Facades\Http;
 use App\Models\Characters;
+use App\Models\JobStatus;
 
 class InsertCharactersJob implements ShouldQueue
 {
     use Queueable;
     public $tries = 3;
     public $timeout = 120;
+    protected $job_id;
     protected $url;
 
     /**
      * Create a new job instance.
      */
-    public function __construct($url)
+    public function __construct($job_id, $url)
     {
+        $this->job_id = $job_id;
         $this->url = $url;
     }
 
@@ -30,9 +33,21 @@ class InsertCharactersJob implements ShouldQueue
     {
         // clear table
         DB::table('characters')->delete();
-        
+
+        // insert job status into jobStatus table
+        JobStatus::updateOrCreate(
+            ['job_id' => $this->job_id],
+            ['status' => 'processing']
+        );
+
         //get characters from external rickmorty API and insert into database
         $this->getCharacters($this->url);
+
+        // update job status for client side
+        JobStatus::updateOrCreate(
+            ['job_id' => $this->job_id],
+            ['status' => 'completed']
+        );
     }
 
     protected function getCharacters(string $url): void
@@ -40,7 +55,7 @@ class InsertCharactersJob implements ShouldQueue
         $response = Http::withOptions(['verify' => false])->get($url);
         $json = $response->json();
         $results = $json['results'];
-        foreach ($results as $result) {
+        foreach ($results as $key => $result) {
             $result['location'] = intval(str_replace('https://rickandmortyapi.com/api/location/', '', $result['location']['url']));
             $result['episode'] = str_replace('https://rickandmortyapi.com/api/episode/', '', $result['episode']);
             foreach ($result['episode'] as $key => $episode) {
