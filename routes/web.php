f5e41e2d8ac148b4a57abfd37caf0a838e111d68
gitName <bartk@list.ru>
2025-04-08 15:57:01 +0300

    feat: add /jobstatus endpoint to check finished inserting jobs

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RickMortyController;
+use App\Http\Controllers\JobStatusController;
 use App\Http\Controllers\ExportController;
 
 Route::get('/', [HomeController::class, 'index'])->name('root');
 Route::post('/', [HomeController::class, 'index']);
 Route::get('/rickmortyapi', [RickMortyController::class, 'store'])->name('rickmortyapi.store');
+Route::get('/jobstatus/{id}', [JobStatusController::class, 'check'])->name('jobstatus.check');
 Route::post('/export', [ExportController::class, 'export']);
