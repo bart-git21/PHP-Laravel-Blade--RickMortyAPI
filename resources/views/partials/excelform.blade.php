8e6240ba03284a1533fc517214c185398a38f820
gitName <bartk@list.ru>
2025-04-09 14:49:14 +0300

    refactor: add button to create downodable excel link with all stored characters

--- resources/views/partials/excelform.blade.php
+++ resources/views/partials/excelform.blade.php
@@ -1,30 +1,39 @@
-<form id="excelForm" class="mb-3" action="" method="">
-    <button id="exportBtn" type="submit" class="btn btn-outline-success">
-        <img src="{{ asset('images/icons8-excel-48.png') }}" alt="">Export to Excel
+<form id="excelForm" class="mb-3 d-flex flex-column" action="" method="">
+    <button id="exportAllToExcelBtn" type="submit" class="btn btn-outline-success">
+        <img src="{{ asset('images/icons8-excel-48.png') }}" alt="">All data export to Excel
+    </button>
+    <button id="exportFilteredToExcelBtn" type="submit" class="btn btn-outline-success">
+        <img src="{{ asset('images/icons8-excel-48.png') }}" alt="">Filtered data export to Excel
     </button>
 
     <script>
-        $(document).ready(function () {
-            $('#exportBtn').on('click', function (event) {
-                event.preventDefault();
-                const characters = @json($characters);
-                $.ajax({
-                    url: '/export',
-                    method: "POST",
-                    data: JSON.stringify({ table: characters }),
-                    contentType: 'application/json',
-                    headers: {
-                        'X-CSRF-TOKEN': "{{ csrf_token() }}"
-                    },
+        function createExcelDownloadableLink(charactersList, excelLinkTitle) {
+            $.ajax({
+                url: '/export',
+                method: "POST",
+                data: JSON.stringify({ table: charactersList }),
+                contentType: 'application/json',
+                headers: {
+                    'X-CSRF-TOKEN': "{{ csrf_token() }}"
+                },
+            })
+                .done(response => {
+                    const link = document.createElement('a');
+                    link.href = 'data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,' + response.fileData;
+                    link.download = 'filtered_characters.xlsx';
+                    link.textContent = excelLinkTitle;
+                    $("#excelForm").append(link);
                 })
-                    .done(response => {
-                        const link = document.createElement('a');
-                        link.href = 'data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,' + response.fileData;
-                        link.download = 'filtered_characters.xlsx';
-                        link.textContent = 'download';
-                        $("#excelForm").append(link);
-                    })
-                    .fail()
+                .fail()
+        }
+        $(document).ready(function () {
+            $('#exportAllToExcelBtn').on('click', function (event) {
+                event.preventDefault();
+                createExcelDownloadableLink(@json($allCharacters), 'download all');
+            })
+            $('#exportFilteredToExcelBtn').on('click', function (event) {
+                event.preventDefault();
+                createExcelDownloadableLink(@json($filteredCharacters), 'download filtered characters')
             })
         })
     </script>
