f24f5e1fb9f72d93773c530a138b86ee57d9de25
gitName <bartk@list.ru>
2025-04-09 15:48:45 +0300

    refactor: separate the pagination forms from resulttable.blade

--- resources/views/partials/resulttable.blade.php
+++ resources/views/partials/resulttable.blade.php
@@ -8,39 +8,6 @@
     }
 </style>
 
-<div class="w-100 d-flex justify-content-around" id="arrows">
-    @if ($offset > 0)
-        <form class="form-content" action="/" method="post">
-            @csrf
-            <input type="hidden" name="name" value="{{ $name }}">
-            <input type="hidden" min="1" max="51" name="episode" value="{{ $episode }}">
-            <input type="hidden" name="location" value="{{ $location }}">
-            <input type="hidden" name="options" value="{{ $options }}">
-            <input type="hidden" name="options" value="{{ $options }}">
-            <input type="hidden" name="offset" value="{{ $offset - 1 }}">
-            <button type="submit" class="btn mb-3">
-                <img width="40" height="40" src="https://img.icons8.com/flat-round/64/long-arrow-left.png"
-                    alt="long-arrow-left" />
-            </button>
-        </form>
-    @endif
-    @if (($offset + 1) * $step < count($filteredCharacters))
-        <form class="form-content" action="/" method="post">
-            @csrf
-            <input type="hidden" name="name" value="{{ $name }}">
-            <input type="hidden" min="1" max="51" name="episode" value="{{ $episode }}">
-            <input type="hidden" name="location" value="{{ $location }}">
-            <input type="hidden" name="options" value="{{ $options }}">
-            <input type="hidden" name="options" value="{{ $options }}">
-            <input type="hidden" name="offset" value="{{ $offset + 1 }}">
-            <button type="submit" class="btn mb-3">
-                <img width="40" height="40" src="https://img.icons8.com/flat-round/64/long-arrow-right.png"
-                    alt="long-arrow-right" />
-            </button>
-        </form>
-    @endif
-</div>
-
 <table class="table table-dark table-striped table-hover table-bordered border-primary">
     <thead class="table-light text-center fs-5">
         <th>id</th>
