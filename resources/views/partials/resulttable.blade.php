a4597222efe1ecdb49df142a04ab7e475f2ac0df
gitName <bartk@list.ru>
2025-04-10 18:38:42 +0300

    refactor: add condition to define is logged user: if logged, assign favorite characters

--- resources/views/partials/resulttable.blade.php
+++ resources/views/partials/resulttable.blade.php
@@ -32,9 +32,10 @@
                         <img class="card-img-top position-relative" src="{{ $single->img_href }}" alt="">
                         <div class="position-absolute end-0">
                             @php
-                                $favorite = (DB::table('favorite_characters')->where('user_id', $user_id)->where('character_id', $single->character_id)->exists()) ? 'favorite' : ''
+                                $favorite = $user_id && (DB::table('favorite_characters')->where('user_id', $user_id)->where('character_id', $single->character_id)->exists()) ? 'favorite' : ''
                             @endphp
-                            <svg class="{{ $favorite }}" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none">
+                            <svg class="{{ $favorite }}" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                fill="none">
                                 <path fill="white"
                                     d="M7 5a4 4 0 0 0-4 4c0 3.552 2.218 6.296 4.621 8.22A21.5 21.5 0 0 0 12 19.91a21.6 21.6 0 0 0 4.377-2.69C18.78 15.294 21 12.551 21 9a4 4 0 0 0-4-4c-1.957 0-3.652 1.396-4.02 3.2a1 1 0 0 1-1.96 0C10.652 6.396 8.957 5 7 5">
                                 </path>
