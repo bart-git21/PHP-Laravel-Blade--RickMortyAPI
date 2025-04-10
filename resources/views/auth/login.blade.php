c6d9b96ea11882d7fb130594f375752846cab961
gitName <bartk@list.ru>
2025-04-10 18:37:30 +0300

    feat: add login blade page

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.html')
+
+@section('content')
+    <h2>Log in</h2>
+    <form id="loginForm" action="" method="POST">
+        @csrf
+        <div>
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" required autofocus>
+        </div>
+        <div>
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" required>
+        </div>
+        <button type="submit">Log in</button>
+    </form>
+    <script>
+        $(document).ready(function () {
+            $("#loginForm").on("submit", function (event) {
+                event.preventDefault();
+                console.log($(this).serialize());
+                $.ajax({
+                    url: "",
+                    method: "POST",
+                    data: $(this).serialize(),
+                    headers: {
+                        "X-CSRF-TOKEN": "{{ csrf_token() }}"
+                    }
+                })
+                    .done((response) => {
+                        window.location.href = "/";
+                    })
+                    .fail((xhr, status, error) => {
+                        console.error(xhr.responseText);
+                    })
+            })
+        })
+    </script>
+@endsection
