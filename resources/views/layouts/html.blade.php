8edc0d25e7f029297109a745ce060dad9f788e6a
gitName <bartk@list.ru>
2025-04-10 19:44:20 +0300

    refactor: include header component into the html layout

--- resources/views/layouts/html.blade.php
+++ resources/views/layouts/html.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title')</title>
-    
+
     <!-- jQuery -->
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"
         integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
@@ -21,6 +21,7 @@
 </head>
 
 <body>
+    @include('partials.header')
     @yield('content')
     @include('partials.footer')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
