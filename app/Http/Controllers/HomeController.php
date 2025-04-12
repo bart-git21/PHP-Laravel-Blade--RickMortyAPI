0fd5e810557fff903d5a125c60d3a2e9f06b9c4d
gitName <bartk@list.ru>
2025-04-12 23:12:10 +0300

    refactor: define view options in the controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,33 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Characters;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
     public function index(Request $request)
     {
-        if (
-            $request->offset
-            || $request->name
-            || $request->episode
-            || $request->options
-            || $request->location
-        ) {
-            $offset = $request->offset ?: 0;
-            return view('home', [
-                'offset' => $offset,
-                'name' => $request->name,
-                'episode' => $request->episode,
-                'options' => $request->options,
-                'location' => $request->location
-            ]);
-        }
+        $offset = $request->offset ?: 0;
+        $name = $request->name ?: '';
+        $episode = $request->episode ?: '';
+        $options = $request->options ?: '';
+        $location = $request->location ?: '';
+        $step = 10;
+        $characters = new Characters();
+        $allCharacters = $characters->getAllCharacters();
+        $paginatedCharacters = $characters->getPaginatedCharacters($offset, $step, $name, $options, $episode, $location);
+        $filteredCharacters = $characters->getFilteredCharactes($name, $options, $episode, $location);
+
         return view('home', [
-            'offset' => 0,
-            'name' => "",
-            'episode' => "",
-            'options' => "",
-            'location' => ""
+            'allCharacters' => $allCharacters,
+            'filteredCharacters' => $filteredCharacters,
+            'paginatedCharacters' => $paginatedCharacters,
+            'allCharactersCount' => $allCharacters->count(),
+            'offset' => $offset,
+            'step' => $step,
+            'filteredCharactersCount' => $filteredCharacters->count(),
+            'name' => $name,
+            'episode' => $episode,
+            'options' => $options,
+            'location' => $location,
+            'user_id' => Auth::id(),
         ]);
     }
 }
