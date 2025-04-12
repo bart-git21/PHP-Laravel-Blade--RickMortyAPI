5a112717b59e7b62cf95e1dfadad879e83e6d88f
gitName <bartk@list.ru>
2025-04-12 22:35:37 +0300

    refactor: relocate getting characters functions from home.blade to characters model

--- app/Models/Characters.php
+++ app/Models/Characters.php
@@ -13,4 +13,44 @@ class Characters extends Model
     protected $casts = [
         'episodes_id' => 'array'
     ];
+
+    public function getAllCharacters()
+    {
+        return Characters::
+            select('characters.*', 'locations.location_name')
+            ->leftJoin('locations', 'characters.location_id', '=', 'locations.location_id')
+            ->get();
+    }
+
+    public function getFilteredCharactes($name, $options, $episode, $location)
+    {
+        return Characters::
+            where('name', 'LIKE', "%$name%")
+            ->where('status', 'LIKE', "%$options%")
+            ->when($episode, function ($query) use ($episode) {
+                return $query->whereRaw('JSON_CONTAINS(episodes_id, CAST(? AS JSON))', [$episode]);
+            })
+            ->select('characters.*', 'locations.location_name')
+            ->leftJoin('locations', 'characters.location_id', '=', 'locations.location_id')
+            ->where('location_name', 'LIKE', "%$location%")
+            ->orderBy('character_id', 'asc')
+            ->get();
+    }
+
+    public function getPaginatedCharacters($offset, $step, $name, $options, $episode, $location)
+    {
+        return Characters::
+            where('name', 'LIKE', "%$name%")
+            ->where('status', 'LIKE', "%$options%")
+            ->when($episode, function ($query) use ($episode) {
+                return $query->whereRaw('JSON_CONTAINS(episodes_id, CAST(? AS JSON))', [$episode]);
+            })
+            ->select('characters.*', 'locations.location_name')
+            ->leftJoin('locations', 'characters.location_id', '=', 'locations.location_id')
+            ->where('location_name', 'LIKE', "%$location%")
+            ->orderBy('character_id', 'asc')
+            ->offset($offset * $step)
+            ->limit($step)
+            ->get();
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,42 +3,9 @@
 @section('title', 'rick morty application')
 
 @section('content')
-    @php
-        $user_id = Auth::id();
-        $step = 10;
-        $allCharacters = DB::table('characters')
-            ->select('characters.*', 'locations.location_name')
-            ->leftJoin('locations', 'characters.location_id', '=', 'locations.location_id')
-            ->get();
-        $filteredCharacters = DB::table('characters')
-            ->where('name', 'LIKE', "%$name%")
-            ->where('status', 'LIKE', "%$options%")
-            ->when($episode, function ($query) use ($episode) {
-                return $query->whereRaw('JSON_CONTAINS(episodes_id, CAST(? AS JSON))', [$episode]);
-            })
-            ->select('characters.*', 'locations.location_name')
-            ->leftJoin('locations', 'characters.location_id', '=', 'locations.location_id')
-            ->where('location_name', 'LIKE', "%$location%")
-            ->orderBy('character_id', 'asc')
-            ->get();
-        $paginatedCharacters = DB::table('characters')
-            ->where('name', 'LIKE', "%$name%")
-            ->where('status', 'LIKE', "%$options%")
-            ->when($episode, function ($query) use ($episode) {
-                return $query->whereRaw('JSON_CONTAINS(episodes_id, CAST(? AS JSON))', [$episode]);
-            })
-            ->select('characters.*', 'locations.location_name')
-            ->leftJoin('locations', 'characters.location_id', '=', 'locations.location_id')
-            ->where('location_name', 'LIKE', "%$location%")
-            ->orderBy('character_id', 'asc')
-            ->offset($offset * $step)
-            ->limit($step)
-            ->get();
-    @endphp
-
     <div class="container">
         <h1 class="text-center my-3">Rick & Morty Characters</h1>
-        @if (DB::table('characters')->count() > 0)
+        @if ($allCharactersCount > 0)
             <div class="row">
                 <div class="col-4"></div>
                 <div class="col-8">
