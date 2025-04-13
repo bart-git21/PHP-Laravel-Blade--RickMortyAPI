b5edb8b8e36c70048d2d49496e308fbe8ec83db0
gitName <bartk@list.ru>
2025-04-13 23:18:28 +0300

    refactor: use one job; instead of using 3 separated jobs

--- app/Http/Controllers/RickMortyController.php
+++ app/Http/Controllers/RickMortyController.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Str;
-use Illuminate\Support\Facades\Redirect;
 use App\Jobs\InsertCharactersJob;
 use App\Jobs\InsertLocationsJob;
 use App\Jobs\InsertEpisodesJob;
@@ -17,16 +15,9 @@ class RickMortyController extends Controller
     }
     public function store()
     {
-        $locationUrl = 'https://rickandmortyapi.com/api/location/';
-        InsertLocationsJob::dispatch($locationUrl);
-
-        $episodeUrl = 'https://rickandmortyapi.com/api/episode/';
-        InsertEpisodesJob::dispatch($episodeUrl);
-
         $jobId = Str::uuid()->toString();
-        $charactersUrl = 'https://rickandmortyapi.com/api/character/';
-        InsertCharactersJob::dispatch($jobId, $charactersUrl);
-
+        $url = 'https://rickandmortyapi.com/api/';
+        InsertRickMortyApiDataJob::dispatch($url);
         return response()->json(['jobId' => $jobId], 201);
     }
 }
