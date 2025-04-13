dff3cd67b490d6a8dba8861075642cbca5d46fe3
gitName <bartk@list.ru>
2025-04-13 23:41:32 +0300

    refactor: rename InsertLocations job to InsertRickMortyApiData job; delete unnecessary job files

--- app/Jobs/InsertRickMortyApiDataJob.php
+++ app/Jobs/InsertRickMortyApiDataJob.php
@@ -6,22 +6,25 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
 use App\Models\Characters;
 use App\Models\Episodes;
+use App\Models\JobStatus;
 use App\Models\Locations;
 use App\Models\FavoriteCharacters;
 
-class InsertLocationsJob implements ShouldQueue
+class InsertRickMortyApiDataJob implements ShouldQueue
 {
     use Queueable;
 
     public $tries = 1;
-    public $timeout = 120;
+    public $timeout = 300;
     protected $url;
+    protected $job_id;
 
     /**
      * Create a new job instance.
      */
-    public function __construct($url)
+    public function __construct($job_id, $url)
     {
+        $this->job_id = $job_id;
         $this->url = $url;
     }
 
@@ -30,6 +33,12 @@ class InsertLocationsJob implements ShouldQueue
      */
     public function handle(): void
     {
+        // insert job status into jobStatus table
+        JobStatus::updateOrCreate(
+            ['job_id' => $this->job_id],
+            ['status' => 'processing']
+        );
+
         // clear tables
         FavoriteCharacters::clearFavoriteCharactersTable();
         Characters::clearCharactersTable();
@@ -43,5 +52,12 @@ class InsertLocationsJob implements ShouldQueue
         }
         $locations->insertLocationsFromUrl("$this->url/location/");
         (new Episodes())->insertEpisodesFromUrl("$this->url/episode/");
+        (new Characters())->insertCharactersFromUrl("$this->url/character/");
+
+        // update job status for client side
+        JobStatus::updateOrCreate(
+            ['job_id' => $this->job_id],
+            ['status' => 'completed']
+        );
     }
 }
