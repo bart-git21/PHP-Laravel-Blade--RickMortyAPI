b46c43c269fb6b064314d67eb89245f3ba1d1296
gitName <bartk@list.ru>
2025-04-13 23:14:19 +0300

    refactor: separate the episodes table changing methods from job into episodes model

--- app/Models/Episodes.php
+++ app/Models/Episodes.php
@@ -3,18 +3,50 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\DB;
 
 class episodes extends Model
 {
     protected $table = 'episodes';
 
-    protected $fillable = ['eposide_id', 'name', 'url', 'air_date', 'characters'];
+    protected $fillable = ['episode_id', 'name', 'url', 'air_date', 'characters'];
 
     protected $casts = [
         'characters' => 'array',
     ];
-    public static function clearEpisodesTable() {
+    public static function clearEpisodesTable()
+    {
         DB::table('episodes')->truncate();
+    }
+    public function insertEpisode($episode_id, $name, $url, $air_date, $characters)
+    {
+        $data = [
+            'episode_id' => $episode_id,
+            'name' => $name,
+            'url' => $url,
+            'air_date' => $air_date,
+            'characters' => $characters
+        ];
+        return self::create($data);
+    }
+    public function insertEpisodesFromUrl($url)
+    {
+        $response = Http::withOptions(['verify' => false])->get($url);
+        $json = $response->json();
+        $results = $json['results'];
+        foreach ($results as $result) {
+            $result['characters'] = str_replace('https://rickandmortyapi.com/api/character/', '', $result['characters']);
+            foreach ($result['characters'] as $key => $character) {
+                $result['characters'][$key] = intval($character);
+            }
+            $this->insertEpisode($result['id'], $result['name'], $result['url'], $result['air_date'], $result['characters']);
+        }
+
+        // loop during get all data from external API
+        $nextPage = $json['info']['next'];
+        if ($nextPage) {
+            $this->insertEpisodesFromUrl($nextPage);
+        }
     }
 }
