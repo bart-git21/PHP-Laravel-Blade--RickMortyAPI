2b25c236c617169d545d8501ccc2e8fc72b796ba
gitName <bartk@list.ru>
2025-04-13 23:39:50 +0300

    refactor: use $job_id to indicate completed process which called by rickmorty controller and GET /rickmorty endpoint

--- app/Http/Controllers/RickMortyController.php
+++ app/Http/Controllers/RickMortyController.php
@@ -3,21 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Str;
-use App\Jobs\InsertCharactersJob;
-use App\Jobs\InsertLocationsJob;
-use App\Jobs\InsertEpisodesJob;
+use App\Jobs\InsertRickMortyApiDataJob;
 
 class RickMortyController extends Controller
 {
-    public function index()
-    {
-        // 
-    }
     public function store()
     {
         $jobId = Str::uuid()->toString();
         $url = 'https://rickandmortyapi.com/api/';
-        InsertRickMortyApiDataJob::dispatch($url);
+        InsertRickMortyApiDataJob::dispatch($jobId, $url);
         return response()->json(['jobId' => $jobId], 201);
     }
 }
