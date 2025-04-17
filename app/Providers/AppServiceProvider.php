b9cb68281ab5f6c0b7f94be2c02aec0443719735
gitName <bartk@list.ru>
2025-04-17 22:55:35 +0300

    chore: logs sql queries errors

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Database\Events\QueryExecuted;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,7 +24,18 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
-            return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+            return config('app.frontend_url') . "/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+        });
+
+        DB::listen(function (QueryExecuted $query) {
+            try {
+                if ($query->time > 1000) {
+                    Log::channel('sql_error_channel')->warning("Slow sql query: $query->time.");
+                    Log::channel('sql_error_channel')->error('bindings: ' . json_encode($query->bindings));
+                }
+            } catch(\Exception $e) {
+                Log::channel('sql_error_channel')->error('SQL error: '. $e->getMessage());
+            }
         });
     }
 }
